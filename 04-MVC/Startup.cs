555071796f9d2b54cae351fbf7d1edce3e3af5d6
Pawel Myszkowski <pawel.myszkowsky@gmail.com>
2020-03-25 17:46:44 +0100

    add View and Controller before database

--- 04-MVC/Startup.cs
+++ 04-MVC/Startup.cs
@@ -50,7 +50,7 @@ namespace _04_MVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}/{name?}");
             });
         }
     }
