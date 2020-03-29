7a1c05c4fe8de1233e19e5d9618b97a8447a5cf9
Pawel Myszkowski <pawel.myszkowsky@gmail.com>
2020-03-29 23:55:09 +0200

    sufficient done

--- 04-MVC/Models/Recipe.cs
+++ 04-MVC/Models/Recipe.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace _04_MVC.Models
 {
@@ -8,9 +10,11 @@ namespace _04_MVC.Models
         public string Name { get; set; }
         public int Time { get; set; }
         public int Difficulty { get; set; }
+        [Display(Name = "Number of likes")]
         public int NumberOfLikes { get; set; }
         public string Ingredients { get; set; }
         public string Process { get; set; }
+        [Display(Name = "Tips and tricks")]
         public string TipsAndTricks { get; set; }
     }
 }
