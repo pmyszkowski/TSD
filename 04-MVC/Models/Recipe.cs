132c697f175d736be3d11c968dcaa83ad0a82c33
Pawel Myszkowski <pawel.myszkowsky@gmail.com>
2020-03-30 00:37:27 +0200

    add validation

--- 04-MVC/Models/Recipe.cs
+++ 04-MVC/Models/Recipe.cs
@@ -7,12 +7,18 @@ namespace _04_MVC.Models
     public class Recipe
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range(10, 50)]
         public int Time { get; set; }
+        [Range(0,10)]
         public int Difficulty { get; set; }
         [Display(Name = "Number of likes")]
+        [Range(0, Int16.MaxValue)]
         public int NumberOfLikes { get; set; }
         public string Ingredients { get; set; }
+        [Required]
+        [MaxLength(1000)]
         public string Process { get; set; }
         [Display(Name = "Tips and tricks")]
         public string TipsAndTricks { get; set; }
